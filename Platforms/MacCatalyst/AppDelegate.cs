7cb55731ae8dc9994e6f33ff556500a974486986
ccbrowndev <46230726+ccbrowndev@users.noreply.github.com>
2023-10-09 18:34:24 -0400

    Add project files.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace menu
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
