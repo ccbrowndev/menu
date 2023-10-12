0db5ad0630249a3d979ea5cef0e358e4e661aedd
ccbrowndev <46230726+ccbrowndev@users.noreply.github.com>
2023-10-12 19:45:12 -0400

    Add SQLite Dependencies and User, UserList, and UserListItem classes

---
+++ Domain/User.cs
@@ -0,0 +1,11 @@
+﻿using SQLite;
+
+namespace menu.Domain
+{
+    [SQLite.Table("USER")]
+    public class User
+    {
+        [PrimaryKey] public int id {  get; set; }
+        [SQLite.MaxLength(50)] public string name { get; set; }
+    }
+}
---
+++ Domain/UserListItem.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+
+namespace menu.Domain
+{
+    [SQLite.Table("LIST_ITEM")]
+    public class UserListItem
+    {
+        [PrimaryKey] public int id { get; set; }
+        public List<UserListItem> subItems { get; set; }
+        public string text;
+    }
+}
