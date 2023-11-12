d8f35132263fee28572b3329a083de3c02dc1f25
YifeiYang20 <yang3yf@mail.uc.edu>
2023-11-12 13:58:22 -0500

    11/12-1

--- List.xaml.cs
+++ List.xaml.cs
@@ -1,29 +1,56 @@
 using menu.Domain;
 using menu.Services;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace menu;
 
 public partial class List : ContentPage
 {
+    private MenuManager menuManager = new MenuManager();
+    public ObservableCollection<UserList> UserLists { get; set; }
     public List()
     {
         InitializeComponent();
+        UserLists = new ObservableCollection<UserList>();
+        userListsView.ItemsSource = UserLists;
     }
 
     protected override async void OnAppearing()
     {
         base.OnAppearing();
+        await LoadDataAsync();
+    }
 
-        MenuManager menuManager = new MenuManager();
-
+    private async Task LoadDataAsync()
+    {
+        // clean ond data
+        UserLists.Clear();
+        // load new
         var lists = menuManager.GetAllLists();
-        listsCollectionView.ItemsSource = lists; 
+        foreach (var list in lists)
+        {
+            UserLists.Add(list);
+        }
+    }
+
+    private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        if (e.SelectedItem is UserList selectedList)
+        {
+            Navigation.PushAsync(new ListDetails(selectedList.id));
+        }
+
+        ((ListView)sender).SelectedItem = null;
+    }
+
+    private void TextCell_Tapped(object sender, EventArgs e)
+    {
+        // If there is data inserted into the list, change this row to a clickable button
     }
 
     private void OnEmptyRecycleBinClicked(object sender, EventArgs e)
     {
-        MenuManager menuManager = new MenuManager();
         menuManager.EmptyRecycleBin();
     }
 
@@ -37,7 +64,6 @@ public partial class List : ContentPage
             name = listName,
         };
 
-        MenuManager menuManager = new MenuManager();
         var addedList = menuManager.AddUserList(listObject);
 
         menuManager.SaveChanges();
