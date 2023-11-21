e029441326e05eb5bc73d2f2f2cc13773c7305c2
ccbrowndev <46230726+ccbrowndev@users.noreply.github.com>
2023-11-20 20:23:07 -0500

    Menu backup commit

---
+++ Model/UserList.cs
@@ -0,0 +1,11 @@
+﻿namespace menu.Model
+{
+    public class UserList
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+
+        public List<UserListItem> ListItems { get; set; }
+    }
+}
---
+++ Model/UserListItem.cs
@@ -0,0 +1,10 @@
+﻿namespace menu.Model
+{
+    public class UserListItem
+    {
+        public int Id { get; set; }
+        public int ListId { get; set; }
+        public string Text { get; set; }
+        public bool Completed { get; set; }
+    }
+}
