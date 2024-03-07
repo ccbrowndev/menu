150b073f0fd0c0a39cc6d750dee5fe04280275d3
ZLongC <chen3zl@mail.uc.edu>
2024-03-07 18:32:21 -0500

    recycle bin page

---
+++ RecycleBin.xaml.cs
@@ -0,0 +1,31 @@
+using menu.ViewModels;
+
+namespace menu
+{
+    public partial class RecycleBin : ContentPage
+    {
+        public RecycleBin()
+        {
+            InitializeComponent();
+        }
+
+        private void RecoveredClicked(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private async void DeletedClicked(object sender, EventArgs e)
+        {
+            bool answer = await DisplayAlert("Confirm Delete", "Are you sure you want to completely delete these items?", "Yes", "No");
+            if (answer)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
