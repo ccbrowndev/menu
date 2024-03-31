a5bd54ae9e2b889fabdc32b184b4babd131b9d44
ccbrowndev <46230726+ccbrowndev@users.noreply.github.com>
2024-03-31 11:35:08 -0400

    WIP: add fields to azure related models

--- Models/AzureList.cs
+++ Models/AzureList.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace menu.Models
+﻿namespace menu.Models
 {
-    internal class AzureList
+    public class AzureList
     {
+        public string ListId { get; set; }
+        public string ListData { get; set; }
+        public string OriginalSenderId { get; set; }
     }
 }
--- Models/AzureListUpdates.cs
+++ Models/AzureListUpdates.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace menu.Models
+﻿namespace menu.Models
 {
     public class AzureListUpdates
     {
+        public List<AzureList> listUpdates;
     }
 }
