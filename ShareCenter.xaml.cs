e202d9afed8e200584203961ae9669f4b103c1ee
ZLongC <chen3zl@mail.uc.edu>
2024-03-31 19:37:13 -0400

    UI for sharecenter three buttons for share center

--- ShareCenter.xaml.cs
+++ ShareCenter.xaml.cs
@@ -1,3 +1,5 @@
+using static System.Net.Mime.MediaTypeNames;
+
 namespace menu;
 
 public partial class ShareCenter : ContentPage
@@ -6,4 +8,39 @@ public partial class ShareCenter : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void PasteCode(object sender, EventArgs e)
+    {
+        if (Clipboard.HasText)
+        {
+            var text = await Clipboard.GetTextAsync();
+
+            paste.Text = text;
+        }
+        else
+        {
+            await Microsoft.Maui.Controls.Application.Current.MainPage.DisplayAlert("Message", "No code has been added.", "OK");
+        }
+    }
+
+    private async void CancelReq(object sender, EventArgs e)
+    {
+        paste.Text = "";
+
+        await Shell.Current.GoToAsync("//MainPage"); ;
+    }
+
+    private async void ConfirmReq(object sender, EventArgs e)
+    {
+        if (!string.IsNullOrEmpty(paste.Text))
+        {
+            //
+
+            await Shell.Current.GoToAsync("//MainPage");
+        }
+        else
+        {
+            await Microsoft.Maui.Controls.Application.Current.MainPage.DisplayAlert("Message", "No code has been added.", "OK");
+        }       
+    }
 }
